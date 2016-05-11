0b922d57286c90b5445313e7f3d642fb74728531
Matt Karl <matt.karl@jibo.com>
2016-05-11 13:47:50 -0400

    Added method to save JSON files

--- client/js/main.js
+++ client/js/main.js
@@ -2,6 +2,8 @@
 
 require('pixi-animate');
 
+const fs = require('fs');
+const path = require('path');
 const electron = require('electron');
 const remote = electron.remote;
 const clipboard = electron.clipboard;
@@ -18,6 +20,9 @@ const renderer = new Renderer(
     $("#view-canvas")
 );
 
+// Current filename
+let currentName;
+
 // drag-n-drop
 new Droppable($('body'), function(err, file) {
     if (err) {
@@ -31,11 +36,23 @@ new Droppable($('body'), function(err, file) {
 
 // Click on the chooser button to browser for file
 $('#choose').addEventListener('click', function() {
-    dialog.showOpenDialog({ filters: [
-       { name: 'JavaScript', extensions: ['js'] }
-    ]}, function(fileNames){
+    dialog.showOpenDialog({ 
+        filters: [{ name: 'JavaScript', extensions: ['js'] }]
+    }, function(fileNames){
         if (!fileNames) return;
         open(fileNames[0]);
+    });
+});
+
+const save = $("#save");
+save.addEventListener('click', function() {
+    dialog.showSaveDialog({ 
+        title: 'Save Solution',
+        defaultPath: currentName + '.json',
+        filters: [{ name: 'JSON', extensions: ['json'] }]
+    }, function(fileName){
+        if (!fileName) return;
+        fs.writeFileSync(fileName, solution.innerHTML);
     });
 });
 
@@ -58,10 +75,13 @@ solution.addEventListener('click', function() {
 });
 
 function open(file) {
+    save.className = 'disabled';
     renderer.run(file, function(err, result) {
         if (err) {
             return dialog.showErrorBox('Invalid Output Format', err.message);
         }
         solution.innerHTML = JSON.stringify(result, null, '  ');
+        save.className = '';
+        currentName = path.basename(file, '.js');
     });
 }
