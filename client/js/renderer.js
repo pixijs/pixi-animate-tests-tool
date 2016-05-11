bec19dfcacfd4a341f49f2b7ea9138b7e98a6b7f
Matt Karl <matt.karl@jibo.com>
2016-05-11 18:04:44 -0400

    Added soft comparison based on images

--- client/js/renderer.js
+++ client/js/renderer.js
@@ -30,6 +30,10 @@ var Renderer = function(viewWebGL, viewContext2d) {
     this.instance = null;
 };
 
+Renderer.WIDTH = 32;
+Renderer.HEIGHT = 32;
+Renderer.TOLERANCE = 0.01;
+
 // Reference to the prototype
 const p = Renderer.prototype;
 
@@ -75,15 +79,23 @@ p.run = function(file, callback) {
         this.stage.addChild(instance);
         const result = {
             webgl: [],
-            canvas: [] 
+            webglRenders: [],
+            canvas: [],
+            canvasRenders: []
         };
+
         for(let i = 0; i < fla.totalFrames; i++) {
+            let data;
             instance.gotoAndStop(i);
             this.render();
             if (this.hasWebGL) {
-                result.webgl.push(md5(this.webgl.view.toDataURL()));
+                data = this.webgl.view.toDataURL();
+                result.webgl.push(md5(data));
+                result.webglRenders.push(data);
             }
-            result.canvas.push(md5(this.canvas.view.toDataURL()));
+            data = this.canvas.view.toDataURL();
+            result.canvas.push(md5(data));
+            result.canvasRenders.push(data);
         }
         callback(null, result);
     }, path.dirname(file));
@@ -104,26 +116,101 @@ p.compare = function(file, solution, callback) {
             return callback(err);
         }
         if (this.hasWebGL) {
-            if (!this.equals(solution.webgl, result.webgl)) {
-                callback(new Error('WebGL results do not match.'));
+            if (!this.strictEquals(solution.webgl, result.webgl)) {
+                if (!this.softEquals(solution.webglRenders, result.webglRenders)) {
+                    return callback(new Error('WebGL results do not match.'));
+                }
             }
         }
-        if (!this.equals(solution.canvas, result.canvas)) {
-            callback(new Error('Canvas results do not match.'));
+        if (!this.strictEquals(solution.canvas, result.canvas)) {
+            if (!this.softEquals(solution.canvasRenders, result.canvasRenders)) {
+                return callback(new Error('Canvas results do not match.'));
+            }
         }
         callback(null, true);
     });
 };
 
 /**
- * Compare two arrays
- * @method equals
+ * Compare two arrays of images
+ * @method softEquals
  * @private
  * @param {Array} a
  * @param {Array} b
  * @return {Boolean} If we're equal
  */
-p.equals = function(a, b) {
+p.softEquals = function(a, b) {
+    if (a === b) {
+        return true;
+    }
+    if (a === null || b === null) {
+        return false;
+    }
+
+    let length = a.length;
+    if (b.length !== length) {
+        return false;
+    }
+    
+    for (let i=0; i<length; i++) {
+        if (!this.compareImages(a[i], b[i])) {
+            return false;
+        }
+    }
+    return true;
+};
+
+/**
+ * Compare two base64 images
+ * @method compareImages
+ * @param {string} src1
+ * @param {string} src2
+ * @return {Boolean}
+ */
+p.compareImages = function(src1, src2) {
+    var dataA = this.getImageData(src1);
+    var dataB = this.getImageData(src2);
+    let x = 0;
+    let len = dataA.length;
+    let diff = dataA.filter(function(val, i) {
+        return val !== dataB[i];
+    }); 
+    if (diff.length / len > Renderer.TOLERANCE) {
+        return false;
+    }
+    return true;
+};
+
+/**
+ * Get an array of pixels
+ * @method getImageData
+ * @param {string} src
+ * @return {Uint8ClampedArray}
+ */
+p.getImageData = function(src) {
+    const image = new Image();
+    image.src = src;
+    const canvas = document.createElement('canvas');
+    canvas.width = Renderer.WIDTH;
+    canvas.height = Renderer.HEIGHT;
+    const ctx = canvas.getContext('2d', {
+        antialias: false,
+        preserveDrawingBuffer: true
+    });
+    ctx.drawImage(image, 0, 0, Renderer.WIDTH, Renderer.HEIGHT);
+    const imageData = ctx.getImageData(0, 0, Renderer.WIDTH, Renderer.HEIGHT);
+    return imageData.data;
+};
+
+/**
+ * Compare two arrays
+ * @method strictEquals
+ * @private
+ * @param {Array} a
+ * @param {Array} b
+ * @return {Boolean} If we're equal
+ */
+p.strictEquals = function(a, b) {
     if (a === b) {
         return true;
     }
@@ -137,9 +224,7 @@ p.equals = function(a, b) {
     }
 
     for (let i=0; i<length; i++) {
-        a1 = a[i];
-        a2 = b[i];
-        if (!this.equals(a1, a2)) {
+        if (a[i] !== b[i]) {
             return false;
         }
     }
